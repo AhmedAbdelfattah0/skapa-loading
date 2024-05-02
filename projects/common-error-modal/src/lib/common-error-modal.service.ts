3bcde7de2524f45dfa769f6e90e93c79a4182165
Ahmed Abdelfatah <ahmed.m.abdelfttah@gmail.com>
2024-05-02 17:58:19 +0300

    Refactor error modal: Update close button functionality and remove unused translation constant
    Removed unused translation constant and updated the close button functionality in the error modal to improve code cleanliness and maintainability. The close button now triggers a different function. No functional impact.

--- projects/common-error-modal/src/lib/common-error-modal.service.ts
+++ projects/common-error-modal/src/lib/common-error-modal.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { languageConstants } from './translation.constant';
 
 @Injectable({
   providedIn: 'root'
